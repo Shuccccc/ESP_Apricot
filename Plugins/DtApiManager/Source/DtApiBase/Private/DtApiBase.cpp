96342ed4ee128a52f55c463381775aaf77da3137
Shuccccc <3160883003@qq.com>
2025-08-08 17:42:03 +0800

    新建文件 Poi模块

--- Plugins/DtApiManager/Source/DtApiBase/Private/DtApiBase.cpp
+++ Plugins/DtApiManager/Source/DtApiBase/Private/DtApiBase.cpp
@@ -1,6 +1,6 @@
 ﻿#include "DtApiBase.h"
 #include "DtApiPluginConfig.h"
-#include "Developer/Settings/Public/ISettingsModule.h"
+//#include "Developer/Settings/Public/ISettingsModule.h"
 	
 #define LOCTEXT_NAMESPACE "FDtApiBaseModule"
 
@@ -8,23 +8,23 @@
 void FDtApiBaseModule::StartupModule()
 {
 	//注册设置
-	if (ISettingsModule* SettingsModule = FModuleManager::GetModulePtr<ISettingsModule>("Settings"))
+	/*if (ISettingsModule* SettingsModule = FModuleManager::GetModulePtr<ISettingsModule>("Settings"))
 	{
 		SettingsModule->RegisterSettings("Project","Plugins", "ApiManager",
 			LOCTEXT("ApiManager", "ApiManager"),
 			LOCTEXT("ApiManagerDescription", "Settings for ApiManager"),
 			GetMutableDefault<UDtApiPluginConfig>()
 		);
-	}
+	}*/
 	
 }
 
 void FDtApiBaseModule::ShutdownModule()
 {
-	if (ISettingsModule* SettingsModule = FModuleManager::GetModulePtr<ISettingsModule>("Settings"))
+	/*if (ISettingsModule* SettingsModule = FModuleManager::GetModulePtr<ISettingsModule>("Settings"))
 	{
 		SettingsModule->UnregisterSettings("Project", "Plugins", "ApiManager");
-	}
+	}*/
 }
 
 #undef LOCTEXT_NAMESPACE
---
+++ Plugins/DtApiManager/Source/DtApiManager/Public/DtApiManagerSubSystem.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "DtApiManagerSubSystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DTAPIMANAGER_API UDtApiManagerSubSystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+public:
+	
+	
+	virtual bool ShouldCreateSubsystem(UObject* Outer) const override;
+ 
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	
+	virtual void Deinitialize() override;
+
+
+private:
+	
+	
+};
